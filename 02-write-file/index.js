b3f988a32f139bcc593539595a753ad1e9c6c602
VaultDweller13 <grognak13@gmail.com>
2022-11-06 02:10:28 +0400

    feat: add task 2 solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const file = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(file);
+
+stdout.write('Hello there! Input text:\n');
+
+stdin.on('data', chunk => {
+  if (chunk.toString().trim().toLowerCase() === 'exit') process.exit();
+
+  output.write(chunk)
+});
+
+process.on('exit', () => stdout.write('Thank you! Have a nice day.'));
+process.on('SIGINT', () => process.exit());
+
+
+

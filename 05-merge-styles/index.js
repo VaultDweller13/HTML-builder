a4bbda814759e0c5532f71bbdf875e57eac6427b
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2022-11-07 13:06:52 +0400

    feat: add task 5 solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,36 +6,16 @@ const stylesDir = path.join(__dirname, 'styles');
 const resultFile = path.join(projectDir, 'bundle.css');
 const isStylesheet = file => file.isFile() && path.extname(file.name) === '.css';
 
-// async function readFiles() {
-//   const resultArray = [];
-//   const files =  await fs.readdir(stylesDir, {withFileTypes: true});
+async function readFilesContents() {
+  return (await fs.readdir(stylesDir, {withFileTypes: true}))
+    .filter(isStylesheet)
+    .map((file) => fs.readFile(path.join(stylesDir, file.name), 'utf-8'));
+}
 
-//   files.filter(isStylesheet).forEach(async file => {
-//     const src = path.join(stylesDir, file.name);
-//     const content = await fs.readFile(src, 'utf-8');
-//     resultArray.push(content);
-//   });
+async function writeArray(arr) {
+  const data = (await Promise.all(await arr)).join('');
 
-//   return resultArray;
-// }
+  fs.writeFile(resultFile, data, 'utf-8');
+}
 
-// fs.readdir(stylesDir, {withFileTypes: true})
-//   .then(res => {
-//     // const resultArray = [];
-
-//     res.filter(isStylesheet).forEach(getFileContent);
-//     // .then(content => resultArray.push(content));
-//     return resultArray;
-//   })
-//   .then((res) => {
-//     fs.mkdir(projectDir, {recursive: true});
-//     return res;
-//   })
-//   .then((res) => {
-//     // console.log(data);
-//     console.log(res);
-
-//     // const data = new Uint8Array(Buffer.from(resultArray));
-//     fs.writeFile(resultFile, resultArray, 'utf-8');
-//   });
-  
+writeArray(readFilesContents());
